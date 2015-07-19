8adf00c5c47d0d5036529cf686e15d9935d112f5
shubaivan <shuba.ivan@yandex.ru>
2015-07-19 13:34:12 +0300

    Work for template, add PostController

---
+++ src/PillsBundle/Controller/PostController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace PillsBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method as Method;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ *
+ * @Route("/")
+ */
+class PostController extends Controller
+{
+    /**
+     * @Route("/", name="get_all_posts")
+     * @Method({"GET"})
+     * @Template()
+     * @param Request $request
+     * @return array
+     */
+    public function indexAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $posts = $em->getRepository('PillsBundle:Post')->findBy([], ['id' => 'DESC']);
+
+//        return array(
+//            "posts" => $posts,
+//        );
+        $paginator  = $this->get('knp_paginator');
+        $posts = $paginator->paginate(
+            $posts,
+            $request->query->get('page', 1),
+            4
+        );
+
+        return [
+            'posts'=>$posts,
+            'user' => $this->get('user')->getInfo(),
+        ];
+    }
+}
