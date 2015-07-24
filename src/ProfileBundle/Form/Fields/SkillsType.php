3411bc74879bb5c001cd99eeaad5824602e9dbed
shubaivan <shuba.ivan@yandex.ru>
2015-07-24 22:41:27 +0300

    add form

---
+++ src/ProfileBundle/Form/Fields/SkillsType.php
@@ -0,0 +1,46 @@
+<?php
+namespace ProfileBundle\Form\Fields;
+
+use Symfony\Component\Form\AbstractType,
+    Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Doctrine\ORM\EntityManager;
+
+class SkillsType extends AbstractType
+{
+    private $em;
+
+    public function setEntityManager(EntityManager $em)
+    {
+        $this->em = $em;
+    }
+
+    public function getSkills()
+    {
+        $skills = $this->em->getRepository('ProfileBundle:Skill')->findAll();
+        $new_skills = array();
+
+        foreach($skills as $skill)
+            $new_skills[$skill->getSkill()] = $skill->getSkill();
+
+        return $new_skills;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'choices' => $this->getSkills(),
+            'multiple' => true,
+            'required' => false,
+        ));
+    }
+
+    public function getParent()
+    {
+        return 'choice';
+    }
+
+    public function getName()
+    {
+        return 'skills';
+    }
+}
